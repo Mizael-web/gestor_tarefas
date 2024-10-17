189c6eb70d0586c0f9d66da56b30c68fa7d60b5f
Mizael-web <mizaelmendes@gmail.com>
2024-10-17 16:57:38 -0300

    add. autor1 e autor 2 no index e return no criar autor

--- autor/criarAutor.js
+++ autor/criarAutor.js
@@ -3,7 +3,9 @@ const {autores} = require("./autores");
 function criarAutor(nome, email){
    const novoAutor = {nome, email };
    autores.push(novoAutor);
+   return novoAutor;
 };
+
 //criarAutor("Mizael", "mizaelmendes@gmail.com");
 
 module.exports = {criarAutor };
